5ce3ddfe725bd71d9c5273ef2d38754c86a9835d
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-09-19 12:29:55 -0500

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,22 @@ import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import Head from 'next/head';
+
+// Extend window object to include gtag
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      if (typeof window !== 'undefined' && window.gtag) {
+      if (window.gtag) {
         window.gtag('config', 'G-366GM77WMG', {
           page_path: url,
         });
@@ -26,22 +34,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-366GM77WMG"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-366GM77WMG');
-            `,
-          }}
-        />
+        <title>Sober Tracker</title>
       </Head>
+
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-366GM77WMG`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-366GM77WMG', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Navbar />
       <Component {...pageProps} />
     </>
