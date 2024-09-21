5c0cd57054b5d50f100f349f3f86312024eea9d7
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-09-21 11:51:36 -0500

    u

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,10 +84,9 @@ export default function Home() {
       />
 
       {/* Banner with Modals */}
-   {/* <LoginModal /> */}
-{/* <SignupModal /> */}
-{/* <Banner /> */}
-
+      <Banner />
+      <LoginModal />
+      <SignupModal />
 
       {/* Hero Section */}
       <div className="relative h-screen flex items-center justify-center bg-cover bg-center">
