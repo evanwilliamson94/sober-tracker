67e95ee5f763e9d4588455825fc7e0b1806222d6
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-09-21 12:03:39 -0500

    up

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,6 +2,9 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
+
+
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
