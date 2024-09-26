92f55e8642322a368f933405563f66d5de47aa61
Evan Williamson <78745069+evanwilliamson94@users.noreply.github.com>
2024-09-26 15:37:16 -0500

    img

--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  
-    async headers() {
-      return [
-        {
-          source: '/(.*)', // Applies the caching to all routes
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, max-age=31536000, immutable', // Cache for 1 year
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+
+  // Adding Firebase Storage domain to the list of allowed image domains
+  images: {
+    domains: ['firebasestorage.googleapis.com'], // Add the Firebase Storage domain
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Applies the caching to all routes
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // Cache for 1 year
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
